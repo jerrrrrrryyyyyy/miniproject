1ae1b11c08c4e066d46d0709d7489ead89ffd90e
jerrrrrrryyyyyy <jerinvbrijesh11@gmail.com>
2025-06-11 14:20:36 +0000

    Added user authentification system. Redirection bugs exist.

--- main.py
+++ main.py
@@ -1,13 +1,77 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__, template_folder = 'templates')  # Create a Flask web application
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///complaints.db'  # Configuration of database connection (SQLite)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Turn off modification tracking (improves performance)
+app.config['SECRET_KEY'] = 'groupproject'
 
+# Initializes a database and login manager
 db = SQLAlchemy(app)  
-# Create a database object that will be used to manage the database
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+class Users(UserMixin, db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+# Load user for Flask-Login
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+# Login route
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = Users.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for("/"))
+        else:
+            return render_template("login.html", error="Invalid username or password")
+
+    return render_template("login.html")
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if Users.query.filter_by(username=username).first():
+            return render_template("signup.html", error="Username already taken!")
+
+        hashed_password = generate_password_hash(password, method="pbkdf2:sha256")
+
+        new_user = Users(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for("/"))
+    
+    return render_template("signup.html")
+
+# Home route
+@app.route("/return")
+def home():
+    return render_template("signup.html")
+
+# Logout route
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
 
 # Define a database model for storing complaints
 class Complaint(db.Model):
